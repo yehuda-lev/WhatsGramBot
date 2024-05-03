bf6c0ebb005a7c29c8636b87d30b8906b954c5da
yehuda-lev <94082865+yehuda-lev@users.noreply.github.com>
2024-05-03 13:29:22 +0300

    [db] added tables of `WaUser`, `Topic`, `Message`

---
+++ db/repositoy.py
@@ -0,0 +1,95 @@
+import logging
+import datetime
+
+from sqlalchemy import exists, func
+
+from db.tables import get_session, WaUser, Topic, Message
+
+
+_logger = logging.getLogger(__name__)
+
+
+def create_user_and_topic(wa_id: str, name: str, topic_id: int):
+    """
+    Create user and topic
+    :param wa_id: the number of the user
+    :param name: the name of the user and the topic
+    :param topic_id: the id of the topic
+    :return:
+    """
+    _logger.debug(f"create user wa_id:{wa_id}, name:{name}, topic_id:{topic_id}")
+    with get_session() as session:
+        topic = Topic(
+            topic_id=topic_id,
+            name=name,
+            created_at=datetime.datetime.now(),
+        )
+        user = WaUser(
+            wa_id=wa_id,
+            name=name,
+            topic=topic,
+            created_at=datetime.datetime.now(),
+        )
+
+        session.add_all((user, topic))
+        session.commit()
+
+
+def get_user_by_wa_id(*, wa_id: str) -> WaUser:
+    """
+    Get user by wa_id
+    :param wa_id: the number of the user
+    :return: the user
+    """
+    with get_session() as session:
+        return session.query(WaUser).filter(WaUser.wa_id == wa_id).one()
+
+
+def get_topic_by_topic_id(*, topic_id: int) -> Topic:
+    """
+    Get topic by topic_id
+    :param topic_id: the id of the topic
+    :return: the topic
+    """
+    with get_session() as session:
+        return session.query(Topic).filter(Topic.topic_id == topic_id).one()
+
+
+def create_message(*, wa_id: str, topic_id: int, wa_msg_id: str, topic_msg_id: int):
+    """
+    Create message
+    :param wa_id: the number of the user
+    :param topic_id: the id of the topic
+    :param wa_msg_id: the id of the message in whatsapp
+    :param topic_msg_id: the id of the message in topic
+    :return:
+    """
+    _logger.debug(
+        f"create message wa_id:{wa_id}, topic_id:{topic_id}, wa_msg_id:{wa_msg_id}, topic_msg_id:{topic_msg_id}"
+    )
+    with get_session() as session:
+        user = session.query(WaUser).filter(WaUser.wa_id == wa_id).one()
+        topic = session.query(Topic).filter(Topic.topic_id == topic_id).one()
+        message = Message(
+            wa_msg_id=wa_msg_id,
+            topic_msg_id=topic_msg_id,
+            topic=topic,
+            user=user,
+            created_at=datetime.datetime.now(),
+        )
+
+        session.add(message)
+        session.commit()
+
+
+def get_message(*, topic_msg_id: int | None, wa_msg_id: str | None) -> Message:
+    """
+    Get message by topic_msg_id or wa_msg_id
+    :param topic_msg_id: the id of the message in topic
+    :param wa_msg_id: the id of the message in whatsapp
+    :return: the message
+    """
+    with get_session() as session:
+        if topic_msg_id:
+            return session.query(Message).filter(Message.topic_msg_id == topic_msg_id).one()
+        return session.query(Message).filter(Message.wa_msg_id == wa_msg_id).one()
---
+++ db/tables.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+import logging
+import datetime
+from contextlib import contextmanager
+from sqlalchemy import String, create_engine, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    DeclarativeBase,
+    sessionmaker,
+    relationship,
+)
+
+
+_logger = logging.getLogger(__name__)
+
+engine = create_engine(
+    url="sqlite:///db.sqlite",
+    pool_size=20,
+    max_overflow=10,
+    pool_timeout=30,
+)
+
+Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def get_session() -> Session:
+    """Get session"""
+    new_session = Session()
+    try:
+        yield new_session
+    finally:
+        new_session.close()
+
+
+class BaseTable(DeclarativeBase):
+    pass
+
+
+class WaUser(BaseTable):
+    """Whatsapp user details"""
+
+    __tablename__ = "wa_user"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    wa_id: Mapped[str] = mapped_column(String(15), unique=True)
+    name: Mapped[str] = mapped_column(String(30))
+    active: Mapped[bool] = mapped_column(default=True)
+    banned: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime.datetime]
+    topic_id: Mapped[int] = mapped_column(ForeignKey("topic.id"))
+    topic: Mapped[Topic] = relationship(back_populates="user", lazy="joined")
+    messages: Mapped[list[Message]] = relationship(back_populates="user")
+
+    __table_args__ = (UniqueConstraint("topic_id"),)
+
+
+class Topic(BaseTable):
+    """Topic details"""
+
+    __tablename__ = "topic"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    topic_id: Mapped[int] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(String(30))
+    created_at: Mapped[datetime.datetime]
+    user: Mapped[WaUser] = relationship(back_populates="topic", lazy="joined")
+    messages: Mapped[list[Message]] = relationship(back_populates="topic")
+
+
+class Message(BaseTable):
+    """Message details"""
+
+    __tablename__ = "message"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    topic_msg_id: Mapped[int] = mapped_column(unique=True)
+    wa_msg_id: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime.datetime]
+
+    topic_id: Mapped[int] = mapped_column(ForeignKey("topic.id"))
+    topic: Mapped[Topic] = relationship(back_populates="messages")
+    user_id: Mapped[int] = mapped_column(ForeignKey("wa_user.id"))
+    user: Mapped[WaUser] = relationship(back_populates="messages")
+
+
+BaseTable.metadata.create_all(engine)
